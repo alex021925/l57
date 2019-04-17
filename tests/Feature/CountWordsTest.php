33ce3ab7473d483492dd22b6d4eab64f50d60da3
alejandro <alex021925@gmail.com>
2019-04-17 18:45:25 -0400

    add test

--- tests/Feature/CountWordsTest.php
+++ tests/Feature/CountWordsTest.php
@@ -13,24 +13,69 @@ class CountWordsTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $this->test1([
+
+    public function test1(){
+        $request = [
             "sentence" => "A list of words (words)"
-        ],
-            [
+        ];
+        $responseValue =    [
                 "A"=> 1,
                 "list" => 1,
                 "of"=> 1,
                 "words" => 2
-            ]);
-    }
-
-    protected function test1($request, $responseValue ){
+            ];
         $response = $this->json('POST', 'api/v1/countwords', $request);
-
         $response
             ->assertStatus(200)
             ->assertJson( $responseValue);
     }
+    public function test2(){
+        $request = [
+            "sentence" => "a question? a assertion."
+        ];
+        $responseValue =    [
+            "a"=> 2,"question" => 1,"assertion" =>1
+        ];
+        $response = $this->json('POST', 'api/v1/countwords', $request);
+        $response
+            ->assertStatus(200)
+            ->assertJson( $responseValue);
+    }
+    public function test3(){
+        $request = [
+            "sentence" => "test / test"
+        ];
+        $responseValue =    [
+                "test"=> 2,
+            ];
+        $response = $this->json('POST', 'api/v1/countwords', $request);
+        $response
+            ->assertStatus(200)
+            ->assertJson( $responseValue);
+    }
+    public function test4(){
+        $request = [
+            "sentence" => "Hello (world)!"
+        ];
+        $responseValue =    [
+            "Hello"=>1,"world"=>1
+            ];
+        $response = $this->json('POST', 'api/v1/countwords', $request);
+        $response
+            ->assertStatus(200)
+            ->assertJson( $responseValue);
+    }
+    public function test5(){
+        $request = [
+            "sentence" => ""
+        ];
+        $responseValue =    [
+
+            ];
+        $response = $this->json('POST', 'api/v1/countwords', $request);
+        $response
+            ->assertStatus(200)
+            ->assertJson( $responseValue);
+    }
+
 }
